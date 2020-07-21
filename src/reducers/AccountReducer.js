5b8b85f098ff7dc31a8cb9d38041a320b18e2bb6
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2020-07-21 00:29:19 -0300

    New approach to actions and reducers and logout function

--- src/reducers/AccountReducer.js
+++ src/reducers/AccountReducer.js
@@ -1,5 +1,12 @@
-import { setAccount, setToken, setRefreshToken } from '../../helpers/account';
-import { SIGN_UP } from './SignUpActions';
+import { SIGN_IN, SIGN_UP, SIGN_OUT } from '../actions/AccountActions';
+import {
+  setAccount,
+  setToken,
+  setRefreshToken,
+  removeAccount,
+  removeToken,
+  removeRefreshToken,
+} from '../helpers/account';
 
 const initialState = {
   account: null,
@@ -8,11 +15,12 @@ const initialState = {
 export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
+    case SIGN_IN:
     case SIGN_UP:
       const response = payload ? payload.data : null;
       const account = response ? response.data : null;
       const metadata = response ? response.metadata : null;
-      
+
       const token = metadata ? metadata.token : null;
       const refreshToken = metadata ? metadata.refreshToken : null;
 
@@ -20,8 +28,14 @@ export default function (state = initialState, action) {
       if (token) setToken(token);
       if (refreshToken) setRefreshToken(refreshToken);
 
-      return { ...initialState, account };
+      return { ...state, account };
+    case SIGN_OUT: {
+      removeAccount();
+      removeToken();
+      removeRefreshToken();
+      return { ...initialState, account: null };
+    }
     default:
       return state;
   }
-};
+}
